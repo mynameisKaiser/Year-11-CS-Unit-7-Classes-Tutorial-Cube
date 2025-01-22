74a4f81d935a52656d41b935db9cd83a92041764
mynameisKaiser <kaisertsoi@hotmail.com>
2025-01-22 12:45:36 +0800

    .

---
+++ src/Cube.java
@@ -0,0 +1,34 @@
+public class Cube {
+    //Properties
+    private int side;
+    //Constructors
+    public Cube(){
+        side=1;
+    }
+    public Cube(int side){
+        if (side < 1){
+            throw new IllegalArgumentException("Side length can't be less than 1");
+        }
+        setSide(side);
+    }
+    //Behaviours
+    public int getSide(){
+        return side;
+    }
+    public void setSide(int side){
+        if (side<1){
+            throw new IllegalArgumentException("Side length can't be less than 1");
+        }
+        this.side=side;
+    }
+    public int getSurfaceArea(){
+        int face=side*side;
+        return face*6;
+    }
+    public int getVolume(){
+        return side*side*side;
+    }
+    public String toString(){
+        return "Cube{side="+side+"}";
+    }
+}
